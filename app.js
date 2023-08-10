4cf1f5c5889e68298b052836c879820fe2a97621
MenasheMtku <menashemetuko@gmail.com>
2023-08-10 23:58:49 +0300

    render host

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 require("dotenv").config();
 // app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
